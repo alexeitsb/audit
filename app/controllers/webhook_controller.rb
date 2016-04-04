bc03bc7c5efe0ccf5075c2a3c261039e7c5d9186
Alexei <alexeitsb@gmail.com>
2016-04-04 19:54:23 -0300

    Testing webhook

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -5,16 +5,18 @@ class WebhookController < ApplicationController
     if user = User.find_by_username(params["From"][0..params["From"].index("@")-1])
       if user.email == params["From"]
         description = if params["TextBody"].present?
-          params["TextBody"]
+          params["TextBody"].gsub(/\d+[,]\d+/, "").gsub(/\d{1,2}\/\d{1,2}\/\d{4}/, "").gsub("\r\n", "").rstrip
         else
           "Lançamento enviado por e-mail"
         end
+        value = params["TextBody"].scan(/\d+[,]\d+/).flatten.first || 0
         entity = if params["Subject"].present? && I18n.transliterate(params["Subject"]).downcase == "credito"
           Entry.entities["credit"]
         else
           Entry.entities["debit"]
         end
-        entry = Entry.create(user: user, responsible: user, description: description, value: 0, entity: entity, at: Date.today)
+        at = params["TextBody"].scan(/\d{1,2}\/\d{1,2}\/\d{4}/).flatten.first || Date.today
+        entry = Entry.create(user: user, responsible: user, description: description, value: value, entity: entity, at: at)
       end
     end
     render nothing: true
